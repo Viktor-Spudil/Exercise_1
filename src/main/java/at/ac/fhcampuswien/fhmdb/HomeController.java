ccc682b43ec202db4142bad2d5900c74dbc07c9e
Mert Selcuk <selcukmert@outlook.at>
2023-03-13 20:16:37 +0100

    added first changes based on pair/mob progamming

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -44,18 +44,59 @@ public class HomeController implements Initializable {
         movieListView.setCellFactory(movieListView -> new MovieCell()); // use custom cell factory to display data
 
         // TODO add genre filter items with genreComboBox.getItems().addAll(...)
-        genreComboBox.setPromptText("Filter by Genre");
+        genreComboBox.getItems().addAll("All", "Action", "Comedy", "Drama", "Horror", "Mystery", "Romance", "Thriller");
+        genreComboBox.getSelectionModel().selectFirst();  // Select first item by default
 
         // TODO add event handlers to buttons and call the regarding methods
         // either set event handlers in the fxml file (onAction) or add them here
+        genreComboBox.setOnAction(actionEvent -> {
+            String selectedGenre = genreComboBox.getSelectionModel().getSelectedItem().toString();
+            if (selectedGenre.equals("All")) {
+                // Show all movies
+                observableMovies.clear();
+                observableMovies.addAll(allMovies);
+            } else {
+                // Filter movies by genre
+                ObservableList<Movie> filteredMovies = FXCollections.observableArrayList();
+                for (Movie movie : allMovies) {
+                    if (movie.getGenres().contains(selectedGenre)) {
+                        filteredMovies.add(movie);
+                    }
+                }
+                observableMovies.clear();
+                observableMovies.addAll(filteredMovies);
+            }
+        });
+
+        // Add event handler to search button
+        searchBtn.setOnAction(actionEvent -> {
+            String searchText = searchField.getText().toLowerCase().trim();
+            if (searchText.isEmpty()) {
+                // field is empty = all movies
+                observableMovies.clear();
+                observableMovies.addAll(allMovies);
+            } else {
+                //filter movies by title,description
+                ObservableList<Movie> filteredMovies = FXCollections.observableArrayList();
+                for (Movie movie : allMovies) {
+                    if (movie.getTitle().toLowerCase().contains(searchText) || movie.getDescription().toLowerCase().contains(searchText)) {
+                        filteredMovies.add(movie);
+                    }
+                }
+                observableMovies.clear();
+                observableMovies.addAll(filteredMovies);
+            }
+        });
 
         // Sort button example:
         sortBtn.setOnAction(actionEvent -> {
-            if(sortBtn.getText().equals("Sort (asc)")) {
+            if (sortBtn.getText().equals("Sort (asc)")) {
                 // TODO sort observableMovies ascending
+                observableMovies.sort((o1, o2) -> o1.getTitle().compareTo(o2.getTitle()));
                 sortBtn.setText("Sort (desc)");
             } else {
                 // TODO sort observableMovies descending
+                observableMovies.sort((o1, o2) -> o2.getTitle().compareTo(o1.getTitle()));
                 sortBtn.setText("Sort (asc)");
             }
         });
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -7,10 +7,12 @@ public class Movie {
     private String title;
     private String description;
     // TODO add more properties here
+    private List<String> genres = new ArrayList<>();
 
-    public Movie(String title, String description) {
+    public Movie(String title, String description, List<String> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
 
     public String getTitle() {
@@ -21,9 +23,41 @@ public class Movie {
         return description;
     }
 
+    public List<String> getGenres() {
+        return genres;
+    }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
-        // TODO add some dummy data here
+        List<String> genres1 = new ArrayList<>();
+        genres1.add("Action");
+        genres1.add("Thriller");
+        genres1.add("Adventure");
+
+        Movie movie1 = new Movie("A: The Dark Knight", "A: description", genres1);
+        movies.add(movie1);
+
+        List<String> genres2 = new ArrayList<>();
+        genres2.add("Comedy");
+        genres2.add("Romance");
+
+        Movie movie2 = new Movie("B: Avengers", "B: description", genres2);
+        movies.add(movie2);
+
+        List<String> genres3 = new ArrayList<>();
+        genres3.add("Drama");
+        genres3.add("Mystery");
+
+        Movie movie3 = new Movie("C: The Shawshank Redemption", "C: description", genres3);
+        movies.add(movie3);
+
+        List<String> genres4 = new ArrayList<>();
+        genres4.add("Horror");
+        genres4.add("Mystery");
+        genres4.add("Thriller");
+
+        Movie movie4 = new Movie("D: Russia mother", "D: description", genres4);
+        movies.add(movie4);
 
         return movies;
     }
