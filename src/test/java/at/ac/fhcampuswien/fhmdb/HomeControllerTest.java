54675f147de4018edfdc8213d8ba1f8fc36e5719
Mert Selcuk <selcukmert@outlook.at>
2023-03-17 16:58:20 +0100

    dasdadasddsadadsaddadads

--- src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,24 +1,14 @@
 package at.ac.fhcampuswien.fhmdb;
 
-import at.ac.fhcampuswien.fhmdb.models.Genres;
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class HomeControllerTest {
-    private HomeController homeController;
-
     public List<Movie> allMovies = Movie.initializeMovies();
-
-    void setUp(){
-        homeController = new HomeController();
-    }
 
     @Test
     void testGenreFilter() {
@@ -30,6 +20,7 @@ class HomeControllerTest {
             assertTrue(movie.getGenres().contains("Action"));
         }
     }
+
     @Test
     void testTextFilter() {
         HomeController controller = new HomeController();
@@ -42,4 +33,48 @@ class HomeControllerTest {
         }
     }
 
+    @Test
+    void testSearchAll() {
+        HomeController controller = new HomeController();
+
+        controller.genreComboBox.setValue("All");
+        controller.searchField.setText("");
+
+        controller.searchBtn.fire();
+
+        assertEquals(allMovies, controller.movieListView.getItems());
+    }
+
+    @Test
+    void testSearchByGenre() {
+        HomeController controller = new HomeController();
+
+        String selectedGenre = "Action";
+        List<Movie> expectedMovies = controller.genreFilter(selectedGenre, allMovies);
+
+        controller.genreComboBox.setValue(selectedGenre);
+        controller.searchField.setText("");
+
+        controller.searchBtn.fire();
+
+        assertEquals(expectedMovies, controller.movieListView.getItems());
+    }
+
+    @Test
+    void testSearchByText() {
+        HomeController controller = new HomeController();
+
+        String searchText = "movie";
+        List<Movie> expectedMovies = controller.textFilter(searchText, allMovies);
+
+        controller.genreComboBox.setValue("All");
+        controller.searchField.setText(searchText);
+
+        controller.searchBtn.fire();
+
+        assertEquals(expectedMovies, controller.movieListView.getItems());
+    }
+
 }
+
+
